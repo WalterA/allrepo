7e45f3ca98420f3f822b98737b7b1566f708dc2e
WalterA <walteralb22@gmail.com>
2024-09-24 13:44:54 +0200

    ok

--- ITSCloud-main/Python/Python5_6/json/stampadiz.py
+++ ITSCloud-main/Python/Python5_6/json/stampadiz.py
@@ -1,3 +1,5 @@
+
+
 def print_dictionary(dData,sRoot):
     for keys , values in dData.items():
         if sRoot != "":
@@ -5,4 +7,5 @@ def print_dictionary(dData,sRoot):
         else:
             print("Trova chiave "+ keys)
         if type(dData[keys]) is dict:
+            
             
