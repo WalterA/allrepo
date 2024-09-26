02df4ff75f57b281baa3e5deb550382d07398afb
WalterA <walteralb22@gmail.com>
2024-09-26 17:49:59 +0200

    ok

--- leetcode-main/leetcode/3043.py
+++ leetcode-main/leetcode/3043.py
@@ -1,19 +1,15 @@
 class Solution:
     def longestCommonPrefix(self, arr1: list[int], arr2: list[int]) -> int:
-        arr1= list(map(str, arr1))
-        for i in arr1:
-        arr2=list(map(str,arr2))
-        lista=[]
-        conta=0
-        for str1, str2 in zip(arr1, arr2):
-            conta = 0
-            for c1, c2 in zip(str1, str2):
-                if c1 == c2:
-                    conta += 1
-                
-            lista.append(conta)
-        return sum(lista)
-            
+        arr1 = list(map(str, arr1))
+        arr2 = list(map(str, arr2))
+        count = 0
+        for x, y in zip(arr1, arr2):
+            if x == y:
+                count += len(x)
+            else:
+                break
+        count += len(arr1[count:])
+        return count
             
 arr1 = [1,10,100]
 arr2 = [1000]
