139d882a261f04feb36ba710a4684282fed07413
WalterA <walteralb22@gmail.com>
2024-09-27 13:09:18 +0200

    ok

--- ITSCloud-main/Python/Python5_6/rest/sever.py
+++ ITSCloud-main/Python/Python5_6/rest/sever.py
@@ -1,13 +1,27 @@
-from flask import Flask , json, request
+from flask import Flask, json, request
 
-api = Flask (__name__)
+api = Flask(__name__)
 
-@api.route ('/add_cottadono', methods=['POST'])
+#mettere una lista di liste dove ogni lista è un cittadino
+
+#la chiave è il codice fiscale
+#add cittadino
+#read cittadino
+#update cittadino
+#delete cittadino
+
+
+@api.route('/add_cittadino', methods=['POST'])
 def GestisciAddCittadino():
-    
-    return
-
-
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        jsonReq = request.json
+        print(jsonReq)
+        #cittadini.append(jsonReq)
+        jsonResp = {"Esito":"200", "Msg":"ok"}
+        return json.dumps(jsonResp)
+    else:
+        return 'Content-Type not supported!'
 
 
 api.run(host="127.0.0.1", port=8080)
