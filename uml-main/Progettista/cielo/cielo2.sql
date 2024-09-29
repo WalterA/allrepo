06af7984e56e156f8e275312cd2bba7a133e3826
WalterA <walteralb22@gmail.com>
2024-09-29 17:02:32 +0200

    cielo2 fallimento totale

--- uml-main/Progettista/cielo/cielo2.sql
+++ uml-main/Progettista/cielo/cielo2.sql
@@ -1,8 +1,8 @@
 --1 Quante sono le compagnie che operano (sia in arrivo che in partenza) nei diversi aeroporti?
-SELECT a.codice, a.nome, COUNT(DISTINCT arr.comp) AS num_companies
-FROM aeroporto a, arrpart arr, arrpart arr1
-where  a.codice = arr.arrivo AND a.codice = arr1.partenza
-GROUP BY a.codice, a.nome;
+SELECT a.codice , a.nome, count(distinct arr.comp)
+from aeroporto a, arrpart arr , arrpart arr1
+where arr.arrivo = a.codice and a.codice=arr1.partenza
+group by a.codice 
 
 --2  Quanti sono i voli che partono dall ’aeroporto ’HTR’ e 
 --hanno una durata di almeno 100 minuti?
@@ -12,3 +12,13 @@ where arr.partenza = 'HTR' and arr.codice=v.codice and v.durataminuti<=100
 
 --3  Quanti sono gli aeroporti sui quali opera la compagnia ’Apitalia ’ , per
 --ogni nazione nella quale opera?
+select *
+from arrpart arr,luogoaeroporto la , aeroporto
+where arr.comp= 'Apitalia' 
+--group by la.nazione NON HO CAPITO DOVE OPERA IO HO FRANCIA OLTRE A QUELLI DELLA SOLUZIONE
+
+--4) Qual e’ la media, i l massimo e i l minimo della durata dei voli effettuati
+-- dalla compagnia ’MagicFly’?
+SELECT avg(v.durataminuti), max(v.durataminuti), min(v.durataminuti)
+FROM VOLO v
+WHERE v.comp = 'MagicFly'
