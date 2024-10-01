d4d4d2f8904098e6738de81b832b74583a2b2723
WalterA <walteralb22@gmail.com>
2024-10-01 13:07:40 +0200

    ok

---
+++ leetcode-main/leetcode/1497.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canArrange(self, arr: list[int], k: int) -> bool:
+        index=0
+        index1=1
+        while  index != len(arr):
+            var = arr[index]
+            var1 = arr[index1]
+            somma=var+var1
+            if somma % k == 0:
+                arr.remove(var)
+                arr.remove(var1)
+                index1-=1
+            else:
+                if index1 != len(arr)-1:
+                    index1+=1
+                else:
+                    index+=1
+                    index1=1
+                if index == len(arr):
+                    if arr:
+                        return False
+                    else:
+                        return True
+            
+                    
+arr = [1,2,3,4,5,10,6,7,8,9]
+k = 5
+sol=Solution()
+print(sol.canArrange(arr, k))
+                        
+                
+            
