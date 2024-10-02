9f9b65ea641472556a9c28a88953dd1c67b55f77
WalterA <walteralb22@gmail.com>
2024-10-02 13:54:07 +0200

    ok

--- Java/Terzo/src/Terzo.java
+++ Java/Terzo/src/Terzo.java
@@ -8,12 +8,20 @@ public class Terzo {
 		System.out.println("Terza riga");
 		int pippo ;
 		pippo=100;
-		System.out.println(pippo);
+		System.out.println("Secondo numero " + pippo);
 		int pluto;
 		pluto=200;
+		System.out.println("Primo numero " + pluto);
 		int somma;
 		somma=pippo+pluto;
-		System.out.println(somma);
+		System.out.println("Somma "+somma);
+		int numerone = 2147483647;
+		System.out.println(numerone);
+		numerone=numerone+1;
+		System.out.println(numerone);
+		long numero=224521565;
+		numero=numero+1;
+		System.out.println(numero);
 	}
 
 }
