1aa31653cb6978763f4d2a82459510e86411f464
WalterA <walteralb22@gmail.com>
2024-10-07 00:40:10 +0200

    ok

--- leetcode-main/leetcode/1_two_sum.py
+++ leetcode-main/leetcode/1_two_sum.py
@@ -15,10 +15,10 @@
 # print(twoSum(num,targe))
 
 
-def twoSum(nums: list[int], target: int) -> list[int]:
-        result = [(i,y) for i in range(len(nums)) for y in range(i+1 , len(nums)) if nums[i]+nums[y]==target]
-        if result:
-                return result
+# def twoSum(nums: list[int], target: int) -> list[int]:
+#         result = [(i,y) for i in range(len(nums)) for y in range(i+1 , len(nums)) if nums[i]+nums[y]==target]
+#         if result:
+#                 return result
         # for i in range(len(nums)):
         #         for j in range(i + 1, len(nums)):      
         #                 somma: int = nums[i] + nums[j]
@@ -27,6 +27,22 @@ def twoSum(nums: list[int], target: int) -> list[int]:
                                 
         #                         return [i, j]
         
-num = [2,7,11,15]
-targe = 9
-print(twoSum(num,targe))
+        
+        
+        
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        def ric(nums, target):
+            if not nums:
+                return None
+            ele = nums.pop()
+            for i in nums:
+                if (i + ele) == target:
+                    return [nums.index(i), len(nums)]
+            return ric(nums, target)
+        return ric(nums, target)
+
+num = [3,3]
+targe = 6
+sol=Solution()
+print(sol.twoSum(num,targe))
--- uml-main/Progettista/officina/table.sql
+++ uml-main/Progettista/officina/table.sql
@@ -1,33 +1,43 @@
+CREATE Domain String100_not_null AS String100_not_nullCK (value IS NOT NULL);
+CREATE DOMAIN Int_gz_not_null AS Int_gz_not_nullalue IS NOT NULL and value > 0);
+CREATE TYPE Indirizzo (
+    via String100_not_null,
+    civico Int_gz_not_null
+);
+CREATE DOMAIN codicefiscale AS String100_not_nullCHECK (VALUE ~ '^[A-Z0-9]{16}$');
+
+CREATE DOMAIN numero_di_telefono AS String100_not_nullCHECK (VALUE ~ '^\+?[0-9]{1,15}$');
+
 create TABLE nazione (
-    nome varchar not null,
+    nome String100_not_null,
     primary key  (nome)
 );
 create TABLE regione (
-    nome varchar not null ,
-    nazione varchar not null,
+    nome String100_not_null ,
+    nazione String100_not_null,
     primary key (nome, nazione),
     foreign key (nazione) references nazione(nome)
 );
 create TABLE citta (
-    nome varchar not null,
-    regione varchar not null ,
-    nazione varchar not null,
+    nome String100_not_null,
+    regione String100_not_null ,
+    nazione String100_not_null,
     primary key  (nome, regione, nazione),
     foreign key (regione, nazione) references regione(nome, nazione),
 );
 
-create TABLE marca ( nome varchar not null,
+create TABLE marca ( nome String100_not_null,
 primary key (nome)
 );
 
-create TABLE tipoveicolo(nome varchar not null,
+create TABLE tipoveicolo(nome String100_not_null,
     primary key (nome)
 );
 
 create TABLE modello(
-    nome varchar not null ,
-    marca varchar not null,
-    tipoveicolo varchar not null,
+    nome String100_not_null ,
+    marca String100_not_null,
+    tipoveicolo String100_not_null,
     primary  key (nome),
     foreign key (marca) references marca(nome),
     foreign key (tipoveicolo) references tipoveicolo(nome)
@@ -36,23 +46,23 @@ create TABLE modello(
 
 create TABLE veicolo (
     targa Targa not null,
-    immatricolazione integer not null,
-    modello varchar  not null,
-    marca varchar not null,
-    cliente varchar not null,
+    immatricolazione Int_gz_not_null,
+    modello String100_not_nulll,
+    marca String100_not_null,
+    cliente String100_not_null,
     primary key(targa),
     foreign key (cliente) references cliente(cf)
     foreign key (modello,marca) references modello(nome, marca)
 );
 
 create TABLE officina ( 
-    nome varchar not nulla,
+    nome String100_not_nulla,
     indirizzo Indirizzo not null ,
-    id integer not null ,
-    citta varchar not null ,
-    regione varchar not null,
-    nazione varchar not null,
-    direttore varchar not null,
+    id Int_gz_not_null ,
+    citta String100_not_null ,
+    regione String100_not_null,
+    nazione String100_not_null,
+    direttore String100_not_null,
     primary key (id),
     foreign key (direttore) references direttore (cf)
     foreign  key (citta, regione, nazione) references citta(nome, nazione, regione)
@@ -60,35 +70,36 @@ create TABLE officina (
 
 create TABLE riparazione (
     riconsegna timestamp,
-    codice integer not null,
-    officina integer not null,
+    codice Int_gz_not_null,
+    officina Int_gz_not_null,
     primary key (codice)
     foreign key (officina) references officina (id)
 );
 
 create TABLE persona (
-    cf codFis not null,
-    nome  varchar not null,
+    cf codicefiscale not null,
+    nome  String100_not_null,
     indirizzo Indirizzo not null,
-    telefono varchar not null,
-    citta varchar not null,
-    regione varchar not null,
-    nazione varchar not null,
+    telefono String100_not_null,
+    citta String100_not_null,
+    regione String100_not_null,
+    nazione String100_not_null,
     primary key (cf)
     foreign  key (citta, regione, nazione) references citta(nome, nazione, regione)
 );
 
 create TABLE Cliente (
-    persona cf ,
-    foreign key persona (cf)
+    persona codicefiscale ,
+    foreign key persona references persona(cf)
 );
 create staff (
-    persona cf ,
-    foreign key persona (cf)
+    persona codicefiscale ,
+    primary key (persona)
+    foreign key persona references persona(cf)
 );
 create TABLE dipendente (
-    persona cf ,
-    officina integer not null,
+    persona codicefiscale ,
+    officina Int_gz_not_null,
     lavora date,
     foreign key (officina) references officina (id)
     foreign key persona (cf)
@@ -96,8 +107,9 @@ create TABLE dipendente (
 
 create TABLE direttore (
     nascita date not null,
-    persona cf ,
-    foreign key persona (cf)
+    persona codicefiscale ,
+    primary key (persona)
+    foreign key persona references persona(cf)
 );
 
 
