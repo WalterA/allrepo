7850b47945f74e7ef2772102dd31b497b14a1425
WalterA <walteralb22@gmail.com>
2024-10-10 18:05:33 +0200

    ok

--- ITSCloud-main/Python/Python5_6/rest/bdms.sql
+++ ITSCloud-main/Python/Python5_6/rest/bdms.sql
@@ -8,7 +8,8 @@ CREATE TABLE Cittadini(
     nome VARCHAR(20) NOT NULL,
     cognome VARCHAR(20) NOT NULL,
     data_di_nascita VARCHAR(20) NOT NULL,
-    codice fiscale VARCHAR(20) NOT NULL,
+    codice_fiscale VARCHAR(20) NOT NULL,
     password VARCHAR(20) NOT NULL,
-    PRIMARY KEY (codice fiscale) 
+    PRIMARY KEY (codice_fiscale) 
 );
+psql -h 127.0.0.1 -U postgres postgres
