ddb3a9d5cba247f7e983c5d8475f78d907cfcac8
WalterA <walteralb22@gmail.com>
2024-10-18 01:54:07 +0200

    670

--- leetcode-main/leetcode/670.py
+++ leetcode-main/leetcode/670.py
@@ -1,16 +1,26 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        temp=0
-        stinga= str(num)
-        temp=int(stinga[0])
-        for i in stinga:
-            if temp>int(stinga[0]):
-                stinga[0]=str(temp)
-                break
-            else:
-                temp=int(i)
-        return int(stinga)
+        
+        st = str(num)
+        lista = []
 
+        # Converti ogni cifra in intero e aggiungi alla lista
+        for i in st:
+            lista.append(int(i))
+
+        # Trova il massimo e il suo indice
+        massimo = max(lista)
+        indexM = lista.index(massimo)
+
+        # Se il primo elemento è minore del massimo trovato, esegui lo scambio
+        if lista[0] < lista[indexM]:
+            lista[0], lista[indexM] = lista[indexM], lista[0]
+        
+        # Ritorna il numero come intero
+        return int(''.join(map(str, lista)))
+
+        
+                
 s=Solution()
 print(s.maximumSwap(2736)) # 7236
                 
