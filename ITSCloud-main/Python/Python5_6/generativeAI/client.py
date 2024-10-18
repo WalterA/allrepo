981bb2951a01e5f0d9d6863fa2a27549e4a9e38b
WalterA <walteralb22@gmail.com>
2024-10-18 13:47:18 +0200

    ok

--- ITSCloud-main/Python/Python5_6/generativeAI/client.py
+++ ITSCloud-main/Python/Python5_6/generativeAI/client.py
@@ -1,14 +1,24 @@
 
 import requests, json, sys
+import subprocess
+from myjeson import *
+
 
 base_url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key="
 sGoogleApiKey = "AIzaSyAs5ipps05JoM4G9u2xs7ZOOkYfq-MeZOw"
 api_url = base_url + sGoogleApiKey
 
+def ComponiJsonPerImmagine(sImagePath):
+    subprocess.run(["rm", "./image.jpg"])
+    subprocess.run(["rm", "./request.json"])
+    subprocess.run(["cp", sImagePath,"./image.jpg"])
+    subprocess.run(["bash", "./json.sh"])
+
 print("Benvenuti al Comune - sede locale")
 
 iFlag = 0
 while iFlag==0:
+    
     print("\nOperazioni disponibili:")
     print("1. Inserisci una domanda")
     print("2. Richiedi una domanda su un'immagine")
@@ -20,7 +30,6 @@ while iFlag==0:
         print("Inserisci un numero valido!")
         continue
 
-
     if iOper == 1:
         sDomanda = input("Inserisci domanda: ")
         jsonDataRequest = {"contents": [{"parts":[{"text": sDomanda}]}]}
@@ -31,11 +40,16 @@ while iFlag==0:
             for dRisposta in ListaRisposta:
                 stestorisp=dRisposta['content']['parts'][0]["text"]
                 print(stestorisp)
+                
     elif iOper == 2:
         sImage= input("Inserisci immagine: ")
         sDomanda=input("Inserisci la domanda: ")
-        
-
+        ComponiJsonPerImmagine(sImage)
+        jsonDataRequest= Deserializza("request.json")
+        jsonDataRequest = {"contents": [{"parts":[{"text": sDomanda}]}]}
+        response = requests.post(api_url, json=jsonDataRequest, verify=True)
+        if response.status_code == 200:
+            print(response.json()) 
     elif iOper == 3:
         print("Buona giornata!")
         iFlag = 1
